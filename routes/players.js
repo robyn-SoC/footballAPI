e3bb982607b1a2f1be5eb95661b6e563e3e0c6ab
robyn <robyn.vann95@gmail.com>
2019-04-08 19:54:05 +0100

    get request for singular player, post request, patch request and delete request added

--- routes/players.js
+++ routes/players.js
@@ -14,14 +14,43 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const energiser = new Energiser(req.body);
-//     await energiser.save();
-//     res.status(201).json({ payload: { energiser } });
-//   } catch (err) {
-//     res.status(500).json({ message: "Error creating energiser", error: err });
-//   }
-// });
+router.get("/:id", async (req, res, next) => {
+  try {
+    const player = await Player.find({ID: req.params.id});
+    res.json({ payload: { player } });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error finding player in database", error: err });
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newPlayer = new Player(req.body);
+    await newPlayer.save();
+    res.status(201).json({ message: "player successfully created", payload: { newPlayer } });
+  } catch (err) {
+    res.status(500).json({ message: "Error creating player", error: err });
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const updatePlayer = await Player.findOneAndUpdate({ID: req.params.id}, {$set: req.body}, {upsert: true});
+    res.status(201).json({ message: "player successfully updated", payload: { updatePlayer } });
+  } catch (err) {
+    res.status(500).json({ message: "Error updating player", error: err });
+  }
+})
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletePlayer = await Player.findOneAndDelete({ID: req.params.id});
+    res.status(201).json({ message: "player successfully deleted", payload: { deletePlayer } });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting player", error: err }); 
+  }
+})
 
 module.exports = router;
